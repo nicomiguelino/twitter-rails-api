576f6c613d29c7342d55912f07970d0fd9dbb49f
nicomiguelino <nicomiguelino2014@gmail.com>
2022-06-11 16:10:10 +0800

    Create a new model for user accounts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_11_073900) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_11_080743) do
   create_table "tweets", force: :cascade do |t|
     t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
