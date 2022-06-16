c60739d9b1e3497cf57aa3c45f24033f6dfeb852
nicomiguelino <nicomiguelino2014@gmail.com>
2022-06-16 14:53:38 +0800

    Add a 'name' column to users.
    - This will server as your Twitter display name.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_062552) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_065108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_062552) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
