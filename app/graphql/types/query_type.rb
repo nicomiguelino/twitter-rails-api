b4ed9ea916d640946e4f7cd7f66b63c003dc0ba6
nicomiguelino <nicomiguelino2014@gmail.com>
2022-06-24 21:35:05 +0800

    Modify GraphQL schema.
    - Declare new types and resolvers.
    - Add support for querying users and Tweets.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,30 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :tweets, [TweetType], 'Get all Tweets', null: false
+    field :tweet, TweetType, 'Find a Tweet by ID' do
+      argument :id, ID
+    end
+
+    field :users, [UserType], 'Get all the registered users', null: false
+    field :user, UserType, 'Find a User by ID', null: false do
+      argument :id, ID
+    end
+
+    def tweets
+      Tweet.all
+    end
+
+    def tweet(id:)
+      Tweet.find(id)
+    end
+
+    def users
+      User.all
+    end
+
+    def user(id:)
+      User.find(id)
     end
   end
 end
---
+++ app/graphql/types/tweet_type.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Types
+  class TweetType < Types::BaseObject
+    field :id, ID, null: false
+    field :content, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :user_id, Integer, null: false
+    field :user, UserType, null: false
+  end
+end
